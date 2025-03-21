00b2d53b5a5ace75f2839dc1cbd6c1f93465465f
KR1PT1CS <mogos2625727@gmail.com>
2025-03-21 13:51:51 +0000

    Added LevelTwo

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Spectre.Console;
 
 namespace LevelTwo
 {
-    public class Program
+    public class LevelTwo
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            
+            string imagePath = args.Length > 0 ? args[0] : "tux.jpg";
+            CanvasImage image = new CanvasImage(imagePath);
+            int width = args.Length > 1 ? int.Parse(args[1]) : 24;
+            image.MaxWidth(width);
+            
+            Console.WriteLine(SoGoodLib.SoGoodClass.SoGoodMethod());
+            AnsiConsole.Write(image);
         }
     }
 }
